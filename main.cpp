979e1959c61f8437f34a7a50d0dba0f5ac27a104
OmarxMario <156704505+OmarxMario@users.noreply.github.com>
2025-09-14 23:28:15 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+int main(){
+    system("title math");
+    system("cls");
+    system("color 0c");
+    double a;
+    double b;
+    double c;
+    double s;
+    double e;
+    cout<<"enter at this secuencys ax^2+bx+c";
+    cout<<endl<<"a: ";
+    cin>>a;
+    cout<<endl<<"b: ";
+    cin>>b;
+    cout<<endl<<"c: ";
+    cin>>c;
+    system("cls");
+    system("color 0b");
+    cout<<"good now enter the start,end in this expmple [start,end] "<<endl;
+    cout<<"start: ";
+    cin>>s;
+    cout<<"end: ";
+    cin>>e;
+    for(s;s<=e;s++){
+        cout<<"[x= "<<s<<"] [f(x)="<<((a*(s*s))+(b*s)+(c))<<" ]"<<endl<<endl<<endl;
+
+    }
+    cout<<"=================================="<<endl;
+    cout<<"veritex of curve is : ("<<(-b/2*a)<<" , "<<(a*((-b/2*a)*(-b/2*a))+b*(-b/2*a)+c)<<")"<<endl;
+    if(a>0){
+        cout<<"the minimal value of the function is "<<(a*((-b/2*a)*(-b/2*a))+b*(-b/2*a)+c)<<endl;
+    }
+    else{
+        cout<<"the maximum value of the function is "<<(a*((-b/2*a)*(-b/2*a))+b*(-b/2*a)+c)<<endl;
+    }
+    cout<<"axis of symmetry of the curve is "<<(-b/2*a)<<endl;
+
+    system("pause");
+    return 0;
+
+}
